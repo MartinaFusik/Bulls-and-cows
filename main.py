c79e42385a486e54c62bea6116c2ebe282f0b3f6
MartinaFusik <martina.fusikova@gmail.com>
2022-07-26 23:49:06 +0200

    Start

--- main.py
+++ main.py
@@ -4,3 +4,28 @@ author: Martina Fúsiková
 email: martina.fusikova@gmail.com
 discord: Martina_F#2319
 """
+import random
+
+cara = 47 * "-"
+
+#Generování tajných číslic
+def generuj_cislo():
+    tajne_cislo = range(1000, 10000)
+    unikatni_cislo = random.sample(tajne_cislo, 1)
+    return unikatni_cislo
+
+#Hrajeme
+#porovnávání čísel
+#Vyhodnocení
+#Vyhrál x prohrál
+#Hra začíná - hlavní funkce
+print(
+    "Hi there!",("\n"),
+    cara,("\n"),
+    "I've generated a random 4 digit number for you.",("\n"),
+    "Let's play a bulls and cows game.",("\n"),
+    cara)
+hrac = input("Zadej 4 čísla:")
+print(generuj_cislo())
+
+
