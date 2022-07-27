86da914ac50081bcbf5cb78093b9519f848823ef
MartinaFusik <martina.fusikova@gmail.com>
2022-07-27 14:19:51 +0200

    def kontrola_vstupu

--- main.py
+++ main.py
@@ -17,11 +17,18 @@ def generuj_cislo():
 #vstup od uživatele
 
 def kontrola_vstupu(cisla: str):
+    '''Kontroluje čísla ze vstupu od uživatele.
+    Číslo musí být jen číslo a zároveň se hodnoty ve str
+    nesmějí opakovat'''
+
     zkontrolovany_vstup = []
     for i in cisla:
         if i not in zkontrolovany_vstup:
             if i.isdigit() == True:
+                var = len(zkontrolovany_vstup) == 4
                 zkontrolovany_vstup.append(i)
+                if var is False:
+                    print("Zadej 4 čísla.")
             else:
                 print("Zadej jen čísla!")
         else:
@@ -29,13 +36,32 @@ def kontrola_vstupu(cisla: str):
     return zkontrolovany_vstup
 
 def vstup_s_nulou(cisla: list):
+    '''Kontrolovaný vstup od uživatele nesmí začínat číslem: 0'''
+
     if cisla[0] != "0":
         pass
     else:
         print("Číslo nesmí začínat 0")
 
+def porovnavani(hrac,pocitac):
+    bull_cow = [0, 0]
+    hrac_li = kontrola_vstupu(hrac)
+    for i,j in zip(hrac_li, pocitac):
+        if j in hrac_li:
+            if j == i:
+                bull_cow[0] += 1
+            else:
+                bull_cow[1] += 1
+    return bull_cow
 
 #Hrajeme
+def hrajeme(hrac, pocitac):
+    while hrac == pocitac:
+        bull_cow = porovnavani(hrac, pocitac)
+        print(f"{bull_cow[0]} bulls, {bull_cow[1]} cows")
+        if bull_cow[0] == 4:
+            print("You guessed right!")
+
 #porovnávání čísel
 #Vyhodnocení
 #Vyhrál x prohrál
@@ -49,9 +75,20 @@ def main():
     cara)
     hrac = input("Zadej 4 čísla:")
     pocitac = generuj_cislo()
-    vstup_s_nulou(kontrola_vstupu(hrac))
+    while hrac == pocitac:
+        hrac = input("Zadej 4 čísla:")
+        pocitac = generuj_cislo()
+        porovnavani(hrac, pocitac)
+        bull_cow = porovnavani(hrac, pocitac)
+        print(f"{bull_cow[0]} bulls, {bull_cow[1]} cows")
+        if bull_cow[0] == 4:
+            print("You guessed right!")
+
+
 
 main()
+
+
 
 
 
