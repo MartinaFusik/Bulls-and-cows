b831cf80d9669b1d9c52bd8fccce36d0b0fa9918
MartinaFusik <martina.fusikova@gmail.com>
2022-07-27 10:56:48 +0200

    def kontrola_vstupu

--- main.py
+++ main.py
@@ -14,18 +14,44 @@ def generuj_cislo():
     unikatni_cislo = random.sample(tajne_cislo, 1)
     return unikatni_cislo
 
+#vstup od uživatele
+
+def kontrola_vstupu(cisla: str):
+    zkontrolovany_vstup = []
+    for i in cisla:
+        if i not in zkontrolovany_vstup:
+            if i.isdigit() == True:
+                zkontrolovany_vstup.append(i)
+            else:
+                print("Zadej jen čísla!")
+        else:
+            print("Čísla se nemohou opakovat. Zadej jinou kombinaci.")
+    return zkontrolovany_vstup
+
+def vstup_s_nulou(cisla: list):
+    if cisla[0] != "0":
+        pass
+    else:
+        print("Číslo nesmí začínat 0")
+
+
 #Hrajeme
 #porovnávání čísel
 #Vyhodnocení
 #Vyhrál x prohrál
 #Hra začíná - hlavní funkce
-print(
+def main():
+    print(
     "Hi there!",("\n"),
     cara,("\n"),
     "I've generated a random 4 digit number for you.",("\n"),
     "Let's play a bulls and cows game.",("\n"),
     cara)
-hrac = input("Zadej 4 čísla:")
-print(generuj_cislo())
+    hrac = input("Zadej 4 čísla:")
+    pocitac = generuj_cislo()
+    vstup_s_nulou(kontrola_vstupu(hrac))
+
+main()
+
 
 
